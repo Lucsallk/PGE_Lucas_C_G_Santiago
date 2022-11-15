da3a104f887923dff8ff274adbf2a5320e8dfe68
Lucsallk <lucsallk@hotmail.com>
2022-11-15 12:15:06 -0300

    fix: model cpf = unique

--- restapi/restapi/models.py
+++ restapi/restapi/models.py
@@ -4,7 +4,7 @@ from cpf_field.models import CPFField
 # talvez eu precise remover a uniquicidade de cpf por agora
 class Estagiario(models.Model):
     nomeCompleto = models.CharField(max_length=220)
-    cpf = CPFField(max_length=11)
+    cpf = CPFField(max_length=11, unique=True)
     dataNascimento = models.DateField()
     cursoGrad = models.CharField(max_length=120)
     instEnsino = models.CharField(max_length=220)
