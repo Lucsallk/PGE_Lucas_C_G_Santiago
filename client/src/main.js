5cb716fe77c41e5a95886efa4b4eb8823cb4baf6
Lucsallk <lucsallk@hotmail.com>
2022-11-15 13:20:08 -0300

    feat: config axios e views

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.css";
+import boostrap from "bootstrap/dist/js/bootstrap.bundle.js";
+const app = createApp(App);
 
-import './assets/main.css'
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+app.use(router);
+app.use(boostrap);
+app.mount("#app");
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { getAPI } from "../assets/axios";
+import { onMounted, ref } from "vue";
+
+const estagiario = ref([]);
+const fetchEstagiarios = () => {
+  getAPI
+    .get("/estagiarios/")
+    .then((response) => {
+      console.log("data fetched");
+      console.log(response.data);
+      estagiario.value = response.data;
+    })
+    .catch((err) => console.log(err));
+};
+onMounted(fetchEstagiarios);
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="container mt-5">
+    <div v-for="estagiario in estagiarios" :key="estagiario.id" class="row">
+      <div class="card mb-4 shadow">
+        <p>
+          <span class="fw-semibold">Nome </span> {{ estagiario.nomeCompleto }}
+        </p>
+      </div>
+    </div>
   </main>
 </template>
